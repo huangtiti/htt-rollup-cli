8087e3be13c7f5eeb6960be4d6b3281923bfb6a5
huangtiti <httforgithub@163.com>
2021-06-28 16:46:08 +0800

    docs: add readme

---
+++ README.md
@@ -0,0 +1,5 @@
+快速生成rollup&react&ts模版
+
+使用：
+1. npm i -g htt-rollup-cli
+2. htt-rollup-cli init [你的项目名]
---
+++ index.js
@@ -0,0 +1,46 @@
+#!/usr/bin/env node
+const pkg = require('./package');
+const program = require('commander');
+const chalk = require('chalk');
+const download = require('download-git-repo');
+const inquirer = require('inquirer');
+const ora = require('ora');
+const fs = require('fs');
+const handlebars = require('handlebars');
+
+program
+  .version(pkg.version, '-v, --version')    
+  .command('init <name>')
+  .action((name) => {
+    inquirer.prompt([
+      {
+          type: 'input',
+          name: 'author',
+          message: '请输入作者名'
+      }
+    ]).then((answers) => {
+      const lqProcess = ora('正在创建rollup模版...')
+      lqProcess.start()
+      download('github:huangtiti/rollup-demo#master',
+        name, { clone: true }, (err) => {
+        if (err) {
+          lqProcess.fail('报错啦！！',chalk.red(err));
+        } else {
+          const fileName = `${name}/package.json`
+          const meta = {
+            projectName:name,
+            author: answers.author||'一位不愿透露姓名的大佬'
+          }
+          if (fs.existsSync(fileName)) {
+            const content = fs.readFileSync(fileName).toString()
+            const result = handlebars.compile(content)(meta);
+            fs.writeFileSync(fileName, result)
+          }
+          lqProcess.succeed(chalk.green('成功创建rollup模版！！'));
+        }
+      })
+    })
+})
+
+  
+program.parse(process.argv)
